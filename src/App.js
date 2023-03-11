f2872113d266928a41ff7c294568164510e13e0a
Deepak Singh <singhdeepak5219@gmail.com>
2023-03-11 14:09:04 +0530

    top level structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter} from "react-router-dom";
 import Header from "./components/Header";
-import Feed from "./components/Feed";
-import SearchResult from "./components/SearchResult";
-import VideoDetails from "./components/VideoDetails";
 import { AppContext } from "./context/contextApi";
+import AppRoutes from "./Router/Routes";
+import store from "./Redux/store";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+
+
 
 const App = () => {
     return (
-        <AppContext>
+        <Provider store={store}>
+            <AppContext>
             <BrowserRouter>
+            <Toaster/>
                 <div className="flex flex-col h-full">
                     <Header />
-                    <Routes>
-                        <Route path="/" exact element={<Feed />} />
-                        <Route
-                            path="/searchResult/:searchQuery"
-                            element={<SearchResult />}
-                        />
-                        <Route path="/video/:id" element={<VideoDetails />} />
-                    </Routes>
+                    {/* <LeftNav/> */}
+                    <AppRoutes/>
                 </div>
             </BrowserRouter>
         </AppContext>
+        </Provider>
+        
     );
 };
 
