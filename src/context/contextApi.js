63398480740d444457c72ba96fd432e6eafa6b1a
Deepak Singh <singhdeepak5219@gmail.com>
2023-03-11 14:10:23 +0530

    context api setup

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,37 +1,57 @@
 import React, { createContext, useState, useEffect } from "react";
-
-import { fetchDataFromApi } from "../utils/api";
 export const Context = createContext();
 
 export const AppContext = (props) => {
     const [loading, setLoading] = useState(false);
-    const [searchResults, setSearchResults] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("New");
     const [mobileMenu, setMobileMenu] = useState(false);
+    const [selectedIds,setSelectedIds] = useState([]);
+    const [selectionMode,setSelectionMode] = useState(false);
+    const [moveCategoryId,setMoveCategoryId] = useState('');
+    const [historyCards,setHistoryCards] = useState([]);
 
-    useEffect(() => {
-        fetchSelectedCategoryData(selectedCategory);
-    }, [selectedCategory]);
 
-    const fetchSelectedCategoryData = (query) => {
-        setLoading(true);
-        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
-            console.log(contents);
-            setSearchResults(contents);
-            setLoading(false);
-        });
-    };
+    const toggleSelection = () => {
+        setSelectionMode(!selectionMode);
+    }
+
+    const selectCards = (checked,id) => {
+        if(!checked){
+            setSelectedIds(
+                [...selectedIds,id]
+            )
+            return;
+        }
+        setSelectedIds(selectedIds.filter(_id => _id!==id ));   
+    }
+
+    const selectHistoryCards = (card,date,time) => {
+        const historyCard = {
+            card,date,time
+        }
+        setHistoryCards(
+            [...historyCards,historyCard]
+        )
+    }
 
     return (
         <Context.Provider
             value={{
                 loading,
                 setLoading,
-                searchResults,
                 selectedCategory,
                 setSelectedCategory,
                 mobileMenu,
                 setMobileMenu,
+                selectedIds,
+                selectionMode,
+                toggleSelection,
+                selectCards,
+                setSelectedIds,
+                moveCategoryId,
+                setMoveCategoryId,
+                selectHistoryCards,
+                historyCards
             }}
         >
             {props.children}
