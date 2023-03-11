e2d7a6db9e97a06e0a12f7c0283e6146e0f5b5e3
Deepak Singh <singhdeepak5219@gmail.com>
2023-03-11 17:54:18 +0530

    added key attribute

--- src/components/SelectCategory.jsx
+++ src/components/SelectCategory.jsx
@@ -38,7 +38,7 @@ const SelectCategory = () => {
 <select onChange={handleChange} id="categories" class="w-3/4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
   <option selected>Select Category to Move</option>
   {
-    buckets?.map(bucket => (bucket.id!==id)?(<option value={bucket.id}>{bucket.name}</option>):"")
+    buckets?.map(bucket => (bucket.id!==id)?(<option key={bucket.id} value={bucket.id}>{bucket.name}</option>):"")
   }
 </select>
 <button onClick={handleSubmit} type="button" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Move Cards</button>
