5287035bebbcfa9003723beda25b9731c2b0aa0c
Deepak Singh <singhdeepak5219@gmail.com>
2023-03-11 14:10:44 +0530

    redux implementation

---
+++ src/Redux/ActionTypes/actionTypes.js
@@ -0,0 +1,11 @@
+// Action types for cards
+export const ADD_CARD = 'ADD_CARD';
+export const EDIT_CARD = 'EDIT_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
+export const DELETE_MULTIPLE_CARDS = 'DELETE_MULTIPLE_CARDS';
+export const MOVE_CARDS = "MOVE_CARDS";
+
+// Action types for buckets
+export const ADD_BUCKET = 'ADD_BUCKET';
+export const EDIT_BUCKET = 'EDIT_BUCKET';
+export const DELETE_BUCKET = 'DELETE_BUCKET';
---
+++ src/Redux/Actions/actions.js
@@ -0,0 +1,49 @@
+import { ADD_CARD, DELETE_MULTIPLE_CARDS, EDIT_CARD, DELETE_CARD, ADD_BUCKET, EDIT_BUCKET, DELETE_BUCKET, MOVE_CARDS } from "../ActionTypes/actionTypes";
+
+// Action creator for adding a card
+export const addCard = (bucketId, card) => ({
+    type: ADD_CARD,
+    payload: { bucketId, card },
+  });
+
+// Action creator for editing a card
+export const editCard = (bucketId, cardId, updatedCard) => ({
+    type: EDIT_CARD,
+    payload: { bucketId, cardId, updatedCard },
+  });  
+
+// Action creator for deleting a card
+export const deleteCard = (bucketId, cardId) => ({
+    type: DELETE_CARD,
+    payload: { bucketId, cardId },
+  });  
+
+export const deleteMultipleCards = (bucketId,cardIds) => ({
+    type: DELETE_MULTIPLE_CARDS,
+    payload: { bucketId, cardIds },
+  });  
+
+// Action creator for adding a bucket
+export const addBucket = (bucket) => ({
+    type: ADD_BUCKET,
+    payload: { bucket },
+  });  
+
+// Action creator for editing a bucket
+export const editBucket = (bucketId, updatedBucket) => ({
+    type: EDIT_BUCKET,
+    payload: { bucketId, updatedBucket },
+  });
+  
+// Action creator for deleting a bucket
+export const deleteBucket = (bucketId) => ({
+    type: DELETE_BUCKET,
+    payload: { bucketId },
+  });  
+  
+  export const moveCards = (sourceBucketId, destinationBucketId, cardIds) => {
+    return {
+      type: MOVE_CARDS,
+      payload: {sourceBucketId, destinationBucketId, cardIds}
+    };
+  };  
